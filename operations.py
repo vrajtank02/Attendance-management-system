1ba728adb9ce2dd1ad2b9a5c40b3985ddc316dd5
Vraj tank <vrajtank02@gmail.com>
2026-01-27 22:00:20 +0530

    Added documentation for method

--- operations.py
+++ operations.py
@@ -6,6 +6,7 @@ FILE_NAME = "students_records.csv"
 students_list = []
 
 def load_data():
+    """Loads student data from CSV file into students_list."""
     with open(FILE_NAME, mode='r') as file:
         reader = csv.reader(file)
         for row in reader:
@@ -25,6 +26,7 @@ def load_data():
                 student.mark_attendance(date, status)
 
 def add_new_student():
+    """Adds a new student to the system."""
     class_name = int(input("Enter Class (1 -- 12): "))
 
     count = 0
@@ -47,6 +49,7 @@ def add_new_student():
     print(f"Success! {name} (Roll: {generated_roll_no}) added to Class {class_name}.")
 
 def mark_attendance_today():
+    """Marks attendance for students for today."""
     now = datetime.now()
     date = now.strftime("%d/%m/%Y")
     day_name = now.strftime("%A")
@@ -88,6 +91,7 @@ def mark_attendance_today():
     print("Attendance marked successfully!")
 
 def view_attendance():
+    """Displays attendance history and average attendance for a specific student."""
     target_class = int(input("Enter Class: "))
     
     roll_no = int(input("Enter Roll Number: "))
@@ -111,12 +115,16 @@ def view_attendance():
         print("Student not found in this Class.")
 
 def _find_student(roll_no):
+    """Finds and returns a student by roll number."""
     for s in students_list:
         if s.roll_number == roll_no:
             return s
     return None
 
 def _append_to_file(class_name, r_no, name, date, status):
+    """Appends a new record to the CSV file.
+    ispecially used when adding new students or marking attendance.
+    """
     with open(FILE_NAME, mode='a', newline='') as file:
         writer = csv.writer(file)
         writer.writerow([class_name, r_no, name, date, status])
